ca4c62b0d180f98ad4b4285349082bc4cd599ced
AkshatProg <code.akshat9@gmail.com>
2026-06-12 21:46:41 +0000

    Add array practice files from C relearning

---
+++ arrays/rearr03.c
@@ -0,0 +1,14 @@
+// Take 5 integers and print them in reverse order.
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int i;
+    for(i = 0;i < 5;i++){
+        scanf("%d",&arr[i]);
+
+    } for(i = 4;i>=0;i--){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
